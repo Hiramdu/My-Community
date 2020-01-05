9949844f74c96fe9bb3377eecb079c182dbdbe92
杜高源 <dgy@dugaoyuandeMacBook-Pro.local>
2020-01-05 16:15:00 -0500

    init

---
+++ src/test/java/life/gaoyuan/community/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package life.gaoyuan.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
